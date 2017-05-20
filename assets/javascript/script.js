e4273f685e0a57f14007b568d25c06ca049dcbcb
Steve Andrews <scandrews2@gmail.com>
2017-05-19 21:11:19 -0400

    working project as off midnight thurs 5/19

---
+++ assets/javascript/script.js
@@ -0,0 +1,211 @@
+$(document).ready(function(){
+	// hide city and state unless the user clicks no
+	$("#city").hide();
+	$("#state").hide();
+
+	// sets the user agent for the glassdoor API
+	var userAgent = navigator.userAgent;
+	// object for the job search
+	var queryObject = {
+		city: "",
+		state: "",
+		jobTitle: "programmer"
+	}
+
+	// here is the start of the traitify code
+	Traitify.setPublicKey("9fbbb4e292ade05e28d211f800");
+	Traitify.setHost("https://api-sandbox.traitify.com");
+	Traitify.setVersion("v1");
+	var assessmentId = 'a34e0f60-4a12-45e5-8033-9a7380f4e589';
+	Traitify.ajax("POST","/assessments",function(data){
+		assessmentId = data.id;
+		console.log(data);
+		Traitify.ui.load(assessmentId, ".assessment");
+		var queryURL = "https://api.traitify.com/v1/assessments/a34e0f60-4a12-45e5-8033-9a7380f4e589?data=blend,types,traits,career_matches";
+	},
+	 {'deck_id': 'career-deck'}
+	);
+
+	function displayTraitifyResults (){
+		Traitify.get("/assessments/"+assessmentId+"?data=blend,types,traits,career_matches").then(function(data){
+			console.log(data);
+
+			// Write the outcome to the screen
+			$(".assessment").html("<table><thead><tr><th>Personality Type</th><th>Career</th></tr></thead><tbody>");
+			console.log("array length - " + data.personality_types.length)
+			for (i=0; i < (data.personality_types.length - 1); i++){
+				$(".assessment").append("<tr><td>" + data.personality_types[i].personality_type.name + "</td><td><button class='careerButton btn'>" + data.career_matches[i].career.title + "</button></td></tr>");
+	            };
+			$(".assessment").append("</tbody></table");
+		});
+		whereAmI();
+	// end display Traitify Results	
+	};
+
+
+	function whereAmI(){
+		// This is the geolocator which takes data from the user if the user allows it
+		if ("geolocation" in navigator) {
+			navigator.geolocation.getCurrentPosition(function(position) {
+				console.log(position, position.coords.latitude, position.coords.longitude);
+				var address = getReverseGeocodingData(position.coords.latitude, position.coords.longitude);
+				console.log(address)
+			});
+			// Runs the coordinate data through google api which produces a JSON object filled with location data
+			navigator.geolocation.watchPosition(function(position) {
+				console.log("i'm tracking you!");
+
+				// If geolocating services are declined, show input form for user
+				function name (error) {
+					if (error.code == error.PERMISSION_DENIED)
+						console.log("you denied me");
+						$("#city").show();
+						$("#state").show();
+				};
+			});
+		// close geolocation
+		}
+	// close wherAmI		
+	}
+
+
+	// listener on to scroll down to the traitify
+	$(".introbutton").click(function() {
+	    console.log("got the intro click");
+	    $('html, body').animate({
+	        scrollTop: $(".traitifyAssessment").offset().top
+	    }, 2000);
+	});
+
+	// listener on the "generate traitify careers"
+	$(".continuebutton").on("click", function(){
+		$(".continuebutton").hide();
+		$('html, body').animate({
+	        scrollTop: $(".assessment").offset().top
+	    }, 2000);
+	    console.log("got the submit click");
+	    displayTraitifyResults();
+		
+	});
+
+	// set a listener on a career choise click
+	$(".assessment").on("click", "button", function(){
+		queryObject.jobTitle = $(this).text();
+		queryObject.jobTitle = queryObject.jobTitle.replace(/\s+/g, '');
+		console.log("chosen career - " + queryObject.jobTitle)
+
+	    $('html, body').animate({
+	        scrollTop: $(".results").offset().top
+	    }, 2000);
+
+	// getJobsWithUserDetails(queryObject){
+		// get user input if chose to enter 
+		var inputCity = $(".city").val().trim();
+		var inputState = $(".state").val().trim();
+
+		// if no input (using geoloc services) 
+		if ( !inputCity.length == 0 && !inputState.length == 0 ) {
+			queryObject.city = inputCity;
+			queryObject.state = inputState;
+		}
+
+		var results = getJobsWithUserDetails(queryObject);
+		console.log("results of 'getJobsWithUserDetails' - " + results);
+		// displayResults(results);
+	// };
+	});
+
+
+
+	// This function displays the listing of job openings
+	function displayResults (input){
+		var jobListing = input.response.employers[0].name;
+		console.log("first listed employer - " + jobListing);
+
+		$(".glassdoorResults").html("<table><thead>");
+		$(".glassdoorResults").append("<tr><th class='resultsBloc'>Company</th><th class='resultsBloc'>Job Title</th><th class='resultsBloc'>Location</th></tr></thead>");
+		$(".glassdoorResults").append("<tbody>");
+		for (i=0; i < (input.response.employers.length - 1); i++){
+
+			$(".glassdoorResults").append("<tr><td class='resultsBloc'>" + input.response.employers[i].name + "</td><td class='resultsBloc'>" + input.response.employers[i].featuredReview.jobTitle + "</td><td class='resultsBloc'>" + input.response.employers[i].featuredReview.location + "</td></tr>");
+		};
+		$(".glassdoorResults").append("</tbody></table>");
+	}
+
+	// This function will query the glassdoor API
+	function getJobsWithUserDetails (queryObj){
+		console.log("Query Obj in getJobsWithUserDetails - ", queryObject);
+
+		var myUserAgent = encodeURIComponent(userAgent);
+		console.log("myuseragent - " + myUserAgent);
+
+		// build the query string
+		// var queryURL = "http://api.glassdoor.com/api/api.htm?v=1&format=json&t.p=150175&t.k=f4RgxsRycNk&action=employers&q=IT&userip=50.224.192.194&useragent=Mozilla%2F5.0%20(Windows%20NT%2010.0%3B%20Win64%3B%20x64)%20AppleWebKit%2F537.36%20(KHTML%2C%20like%20Gecko)%20Chrome%2F58.0.3029.110%20Safari%2F537.36&city=" + queryObj.city + "&state=" + queryObj.state + "&q=" + queryObj.jobTitle + "&radius=24&returnJobTitles";
+
+
+		// build the query string
+		var queryURL =  [
+			"http://api.glassdoor.com/api/api.htm?v=1",
+			"format=json",
+			"t.p=150175",
+			"t.k=f4RgxsRycNk",
+			"action=employers",
+			"q=IT",
+			"userip=50.224.192.194",
+			"useragent=" + myUserAgent,
+			"city=" + queryObj.city,
+			"state=" + queryObj.state,
+			"q=" + queryObj.jobTitle,
+			"radius=100",
+			"returnJobTitles"
+		].join("&");
+			// &returnCities&returnStates&returnJobTitles&returnEmployers&pn=1-10"
+
+		// let's see our query string
+		console.log('Query URL: ' + queryURL);
+
+		// call glass door
+		$.ajax({
+			url: queryURL,
+			method: "GET",
+			cors: true,
+			dataType: "jsonp"
+		}).done(function(response) {
+			console.log(response);
+			console.log(response.response);
+
+			displayResults(response);
+			// return response;
+        })
+			// console.log(tempReturn.response.employers[0].name);
+		// return tempReturn;
+	// console.log("outside ajax - " + tempReturn);
+
+	// close function getJobsWithUserDetails
+	}
+
+// Got this code from http://stackoverflow.com/questions/6478914/reverse-geocoding-code
+function getReverseGeocodingData(lat, lng) {
+	var latlng = new google.maps.LatLng(lat, lng);
+	// This is making the Geocode request
+	var geocoder = new google.maps.Geocoder();
+
+	geocoder.geocode({ 'latLng': latlng }, function (results, status) {
+		if (status !== google.maps.GeocoderStatus.OK) {
+			alert(status);
+		}
+		// This is checking to see if the Geoeode Status is OK before proceeding
+		if (status == google.maps.GeocoderStatus.OK) {
+			console.log(results);
+			// Grab the city and state data and set it to the keyvalue pairs referenced within the queryObj variable
+
+			queryObject.city = results[0].address_components[2].short_name;
+			queryObject.state = results[0].address_components[4].short_name;
+		}
+	});
+// end lifted code
+}
+
+
+// end doc ready	
+})
